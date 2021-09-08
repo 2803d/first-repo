7c3e4d6430645f71f861afe9950c2ed9f275ede0
dongyoung0 <wmkimdy@gmail.com>
2021-09-08 17:13:09 +0900

    Merge practice

---
+++ merge/mege.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Merge practice
+# branch : main
+# - 오후 5시 7분
+
+def merge(a, b):
+    '''
+    merge function in main branch
+    '''
+    a = 'main'
+    b = 'main1'
+    print(a+b)
+
+# +
+# How to use jupytext
+# in main branch
+# practice
